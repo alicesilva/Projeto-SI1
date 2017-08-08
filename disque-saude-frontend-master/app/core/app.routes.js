f4f493cf1f1e34942db1e876e51cd0fd41ca9a07
Raquel Ambrozio <raquel.fonseca@ccc.ufcg.edu.br>
2017-08-08 12:14:49 -0300

    páginas de loginEcadastro, falta dividir elas ajeitar umas (várias) coisas, se não for assim me digam kkkk

--- disque-saude-frontend-master/app/core/app.routes.js
+++ disque-saude-frontend-master/app/core/app.routes.js
@@ -1,24 +1,35 @@
 app.config(function ($routeProvider) {
     $routeProvider.when("/",{
         templateUrl: "view/search.html"
+
     }).when("/complaint/register", {
         templateUrl: "view/registerComplaint.html",
         controller : "queixaController",
+
     }).when("/searchcomplaint", {
         templateUrl : "view/search_complaint.html",
         controller: "queixaController"
+
     }).when("/search_health_unit", {
         templateUrl: "view/searchHealthUnit.html",
         controller : "searchHealthUnitCtrl",
+
     }).when("/searchaverage",{
         templateUrl : "view/search_average_per_patient.html",
         controller: "searchAverangeCtrl"
+
     }).when("/createdcomplaint/:id", {
         templateUrl : "view/successPage.html",
         controller : "queixaController"
+
     }).when("/generalSituationComplaints", {
         templateUrl : "view/generalSituationComplaints.html",
         controller : "queixaController"
+
+    }).when("/loginEcadastro", {
+        templateUrl : "loginEcadastro/loginEcadastro.html"
+    })
+        
     }).otherwise({
         redirectTo: '/'
     });
