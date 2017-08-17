1ab866ab425e483ab39ba2bdbeda30997bc06898
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2017-08-17 10:43:13 -0300

    Hospital adapter parametros

--- disque-saude-backend-master/src/main/java/com/ufcg/si1/model/unidadeSaude/HospitalAdaptado.java
+++ disque-saude-backend-master/src/main/java/com/ufcg/si1/model/unidadeSaude/HospitalAdaptado.java
@@ -4,10 +4,40 @@ import br.edu.ufcg.Hospital;
 public class HospitalAdaptado extends UnidadeSaude {
 	
 	private Hospital hospital;
+	private String descricao;
+	private int numMedicos;
+	private float numPacientes;
 	
-	public HospitalAdaptado(Hospital hospital) {
-		super();
+	public HospitalAdaptado(String descricao, int numMedicos, float numPacientes) {
+		//super();
+		this.descricao = descricao;
+		this.numMedicos = numMedicos;
+		this.numPacientes = numPacientes;
 		this.hospital = hospital;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getNumMedicos() {
+		return numMedicos;
+	}
+
+	public void setNumMedicos(int numMedicos) {
+		this.numMedicos = numMedicos;
+	}
+
+	public float getNumPacientes() {
+		return numPacientes;
+	}
+
+	public void setNumPacientes(float numPacientes) {
+		this.numPacientes = numPacientes;
 	}
 	
 }
